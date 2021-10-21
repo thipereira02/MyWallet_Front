71d6ee1142eb354fa81f1f0b6dca8d113b73e2bb
thipereira02 <thiago.spereira02@gmail.com>
2021-10-21 15:41:24 -0300

    Implementing layout of Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import styled from "styled-components";
 import { IoExitOutline } from "react-icons/io5";
+import { BsDashCircle, BsPlusCircle } from "react-icons/bs";
 
 import UserContext from "../contexts/UserContext";
 
@@ -8,16 +9,31 @@ export default function Home() {
     const { userData } = useContext(UserContext);
 
     return (
-        <Box>
-            <Title>
-                Olá, {userData.name}
-            </Title>
-            <IoExitOutline size="32" color="#FFF"/>
-        </Box>
+        <>
+            <Header>
+                <Title>
+                    Olá, {userData.name}
+                </Title>
+                <IoExitOutline size="32" color="#FFF"/>
+            </Header>
+            <RegisterBox>
+                drfhdh
+            </RegisterBox>
+            <Buttons>
+                <Button>
+                    <BsPlusCircle size="20" color="#FFF"/>
+					<p>Nova<br/>entrada</p>
+                </Button>
+                <Button>
+                    <BsDashCircle size="20" color="#FFF"/>
+					<p>Nova<br/>saída</p>
+                </Button>
+            </Buttons>
+        </>
     );
 }
 
-const Box = styled.div`
+const Header = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -29,4 +45,42 @@ const Title = styled.h1`
     font-size: 26px;
     color: #FFF;
     font-weight: bold;
+`;
+
+const RegisterBox = styled.div`
+    height: 446px;
+    background: #FFFFFF;
+    border-radius: 5px;
+    width: 90%;
+    margin: 0 auto;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+const Buttons = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 90%;
+    margin: 13px auto;
+`;
+
+const Button = styled.button`
+    width: 48%;
+    background: #A328D6;
+    border-radius: 5px;
+    height: 114px;
+    border: none;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    padding-top: 10px;
+    padding-left: 10px;
+    p{
+        color: #FFF;
+        font-size: 17px;
+        font-weight: bold;
+        text-align: start;
+    }
 `;
